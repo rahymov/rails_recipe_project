2d40c378263595f23f34604895413b58f836f303
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-05 00:42:35 -0500

    add validations to recipe, category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :recipe_categories
+  has_many :recipes, through: :recipe_categories
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/recip_category.rb
+++ app/models/recip_category.rb
@@ -1,2 +1,4 @@
 class RecipCategory < ApplicationRecord
+  has_many :recipe_categories
+  has_many :categories, through: :recipe_categories
 end
