63945ed0c773d06b13c30727ac0a2ffc17e56363
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-04 22:11:41 -0500

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable,
          :omniauth_providers => [:facebook]
+
    def self.new_with_session(params, session)
      super.tap do |user|
        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
